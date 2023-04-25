f7b132b3f6e8214edee5a86e3c040e5992b92743
algmironov <al.g.mironov@gmail.com>
2023-04-25 16:36:27 +0300

    Migration of Settings Page to MVVM pattern is completed

--- Wpf_GenyiIdiot/Pages/QuestionEdit.xaml.cs
+++ Wpf_GenyiIdiot/Pages/QuestionEdit.xaml.cs
@@ -1,19 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 using Wpf_GenyiIdiot.Model;
 using Wpf_GenyiIdiot.Service;
+using Wpf_GenyiIdiot.ViewModel;
 
 namespace Wpf_GenyiIdiot.Pages
 {
@@ -23,19 +13,30 @@ namespace Wpf_GenyiIdiot.Pages
     public partial class QuestionEdit : Window
     {
         Question question = new Question();
-        public QuestionEdit(Question question)
+        private readonly QuestionsViewModel viewModel;
+        public QuestionEdit(Question question, QuestionsViewModel vm)
         {
             InitializeComponent();
+            viewModel = vm;
             this.question = question;
             OnLoadToEdit();
             this.question = question;
             Title = "Редактирование вопроса";
+            Closed += QuestionEdit_Closed;
         }
 
-        public QuestionEdit()
+        private void QuestionEdit_Closed(object? sender, EventArgs e)
+        {
+            viewModel.UpdateQuestionsCommand.Execute(viewModel.Questions);
+        }
+
+        public QuestionEdit(QuestionsViewModel vm)
         {
             InitializeComponent();
+            viewModel = vm;
             Title = "Добавление вопроса";
+            OnLoadToCreate();
+            Closed += QuestionEdit_Closed;
         }
 
         private void OnLoadToEdit()
@@ -46,13 +47,15 @@ namespace Wpf_GenyiIdiot.Pages
 
         private void OnLoadToCreate()
         {
+            saveDataButton.Content = "Сохранить вопрос";
             questionTextPlace.Text = string.Empty;
+            questionTextPlace.Focus();
             answerTextPlace.Text = string.Empty;
         }
 
         private void OnCloseButton_Click(object sender, RoutedEventArgs e)
         {
-            this.Close();
+            Close();
         }
 
         private void SaveDataButton_Click(object sender, RoutedEventArgs e)
@@ -77,7 +80,7 @@ namespace Wpf_GenyiIdiot.Pages
                     var editedQuestion = new Question(editedQuestionText, newAnswer);
                     QuestionService.EditQuestion(question, editedQuestion);
                     MessageBox.Show("Вопрос успешно отредактирован!", "Успех!", MessageBoxButton.OK);
-                    this.Close();
+                    Close();
                 }
             }
             else
@@ -86,9 +89,22 @@ namespace Wpf_GenyiIdiot.Pages
                 question.Text = editedQuestionText;
                 QuestionService.AddQuestion(question);
                 MessageBox.Show("Вопрос успешно Сохранен!", "Успех!", MessageBoxButton.OK);
+                Close();
             }
             
+            Close();
+        }
 
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                SaveDataButton_Click(sender, e);
+            }
+            if (e.Key == Key.Escape)
+            {
+                Close();
+            }
         }
     }
 }
---
+++ Wpf_GenyiIdiot/ViewModel/QuestionsViewModel.cs
@@ -0,0 +1,111 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+using Wpf_GenyiIdiot.Model;
+using Wpf_GenyiIdiot.Pages;
+using Wpf_GenyiIdiot.Service;
+
+namespace Wpf_GenyiIdiot.ViewModel
+{
+    public partial class QuestionsViewModel : ObservableObject
+    {
+        ObservableCollection<Question> _questions;
+        public ICommand UpdateQuestionsCommand { get; }
+        private Question selectedQuestion;
+        public ObservableCollection<Question> Questions
+        {
+            get { return _questions; }
+            set
+            {
+                _questions = value;
+                OnPropertyChanged(nameof(Questions));
+            }
+        }
+
+        public Question SelectedQuestion
+        {
+            get
+            {
+                return selectedQuestion;
+            }
+
+            set
+            {
+                selectedQuestion = value;
+                OnPropertyChanged(nameof(selectedQuestion));
+            }
+        }
+
+        public QuestionsViewModel()
+        {
+            Questions = new ObservableCollection<Question>(QuestionService.GetQuestions());
+            UpdateQuestionsCommand = new RelayCommand(UpdateQuestionsList);
+        }
+
+        private RelayCommand addQuestionCommand;
+        private RelayCommand deleteQuestionCommand;
+        private RelayCommand editQuestionCommand;
+
+        public IRelayCommand DeleteQuestionCommand => deleteQuestionCommand ??= new RelayCommand(RemoveQuestion);
+        public IRelayCommand AddQuestionCommand => addQuestionCommand ??= new RelayCommand(AddNewQuestion);
+        public IRelayCommand EditQuestionCommand => editQuestionCommand ??= new RelayCommand(EditSelectedQuestion);
+
+        [RelayCommand]
+        private void AddNewQuestion()
+        {
+            QuestionEdit editQuestion = new QuestionEdit(new QuestionsViewModel());
+            SetAndOpenWindow(editQuestion);
+            UpdateQuestionsList();
+            OnPropertyChanged(nameof(Questions));
+        }
+
+        [RelayCommand]
+        private void EditSelectedQuestion()
+        {
+            if (selectedQuestion != null)
+            {
+                QuestionEdit editQuestion = new QuestionEdit(selectedQuestion, new QuestionsViewModel());
+                SetAndOpenWindow(editQuestion);
+                UpdateQuestionsList();
+                OnPropertyChanged(nameof(Questions));
+            }
+        }
+
+        [RelayCommand]
+        private void RemoveQuestion()
+        {
+            if (selectedQuestion != null)
+            {
+                QuestionService.DeleteQuestion(selectedQuestion);
+                Questions.Remove(selectedQuestion);
+            }
+        }
+       
+        private void UpdateQuestionsList()
+        {
+            var newQuestions = QuestionService.GetQuestions();
+            Questions.Clear();
+            foreach (Question question in newQuestions)
+            {
+                Questions.Add(question);
+            }
+        }
+
+        private void SetAndOpenWindow(Window window)
+        {
+            window.Owner = Application.Current.MainWindow;
+            window.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            window.ShowDialog();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
